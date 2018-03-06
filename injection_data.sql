b1740945c15ea25f1275cfe947c48152eb50cd91
Krongrah <Krongrah@gmail.com>
2018-03-06 11:47:32 +0100

    files

---
+++ injection_data.sql
@@ -0,0 +1,10 @@
+create table datastore(
+name varchar(100),
+password varchar(100),
+data varchar(100)
+);
+
+insert into datastore values
+('hans','kanelgiffel','jeg elsker burgere'),
+('petra','momse447','mit cpr er 1234567890'),
+('funnydude88','dawn','kan lide lange ture ved standen');
---
+++ src/dbproject/DBTest.java
@@ -0,0 +1,46 @@
+package dbproject;
+
+import java.sql.*;
+/*
+	Run with:
+	java -cp postgresql-42.2.1.jar:. DBTest
+*/
+
+public class DBTest {
+
+    public static void main(String[] args) {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (java.lang.ClassNotFoundException e) {
+            System.out.println(e);
+        }
+
+
+        String url = "jdbc:postgresql://baasu.db.elephantsql.com:5432/tilfjjea";
+        String username = "tilfjjea";
+        String password = "tC0sMMGx8EH6KyQ7CRBVEIUVzC82C8Zv";
+
+
+
+//e
+
+        try {
+            Connection db = DriverManager.getConnection(url, username, password);
+
+            Statement st = db.createStatement();
+            ResultSet rs = st.executeQuery("select * from users");
+            while (rs.next()) {
+
+                System.out.print(rs.getString(1) + " ");
+                System.out.println(rs.getString(2) + " ");
+            }
+            rs.close();
+            st.close();
+
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+}
