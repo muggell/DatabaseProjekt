1fb46d8a84e9ab48113a4429c1288a68307e2287
Krongrah <Krongrah@gmail.com>
2018-03-07 13:44:39 +0100

    comparator

---
+++ src/dbgui/IgnoreCaseComparator.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dbgui;
+
+import java.util.Comparator;
+
+/**
+ *
+ * @author Krongrah
+ */
+public class IgnoreCaseComparator implements Comparator<String>{
+
+    
+
+    @Override
+    public int compare(String o1, String o2) {
+        return o1.compareToIgnoreCase(o2);
+    }
+    
+}
